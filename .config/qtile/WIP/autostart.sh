9ccc3d2707e0ad9abb405e15834c10d1e034fa9f
5ila5 <silaskoch@gmx.de>
2022-03-28 20:44:54 +0200

    removed useless qtile stuff

--- .config/qtile/WIP/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-#clipmanager &
-#sxhkd &
-#numlockx &
-
--- .config/qtile/WIP/get_core.py
+++
@@ -1,5 +0,0 @@
-from libqtile import qtile
-
-
-def get_core_name() -> str:
-    return qtile.core.name
