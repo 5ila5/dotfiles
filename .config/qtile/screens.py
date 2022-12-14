52c6361fd91aa3a13e2ecbf1ff798d79f4036dc2
5ila5 <5ila5@users.noreply.github.com>
2022-12-14 22:38:28 +0100

    QTILE: Updated vertical border color

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -118,7 +118,8 @@ def init_layouts()->list:
         # layout.RatioTile(),
         # layout.Tile(),
         # layout.TreeTab(),
-        layout.VerticalTile(),
+        layout.VerticalTile(border_normal='#000000',),# border_focus='#009926', border_focus_stack=['#009926', '#009926']),
+
         # layout.Zoomy(),
     ]
     return layouts
