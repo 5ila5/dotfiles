d033d1f219df0af182bb41796878ba85bcf7f51c
5ila5 <5ila5@users.noreply.github.com>
2023-09-11 17:38:31 +0200

    add git aliases

--- .config/qtile/autostart_X11.sh
+++ .config/qtile/autostart_X11.sh
@@ -2,3 +2,5 @@
 if [ -f "/usr/bin/betterlockscreen" ] && [ -d "/usr/share/backgrounds/archlinux" ]; then
 	/usr/bin/betterlockscreen -u /usr/share/backgrounds/archlinux &
 fi
+
+exec dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY &
