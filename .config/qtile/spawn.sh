c386ef46403e52129baf7ce76e6e64dabfd9a735
5ila5 <5ila5@users.noreply.github.com>
2025-04-21 19:17:40 +0200

    spawn script use bemenu if in wayland

--- .config/qtile/spawn.sh
+++ .config/qtile/spawn.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-compgen -c | dmenu -i | bash
+
+if [[ -z $WAYLAND_DISPLAY ]]; then
+  dmenu_run
+else
+  if command -v bemenu-run &>/dev/null; then
+    bemenu-run
+  else
+    dmenu_run
+  fi
+fi
