378e11af3c73e4b233b7152e2246a9319834e5df
5ila5 <5ila5@users.noreply.github.com>
2025-05-13 18:02:37 +0200

    fix desktop autostart not starting libinput-gestures

--- .config/desktop/autostart.sh
+++ .config/desktop/autostart.sh
@@ -2,13 +2,13 @@
 
 # if hyprpolkitagent systemd user agent exists, use it
 if [ -x /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 ]; then
-    /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+  /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 elif systemctl --user list-unit-files | grep -q hyprpolkitagent.service; then
-    if ! systemctl --user is-active hyprpolkitagent.service; then
-        systemctl --user start hyprpolkitagent.service
-    fi
+  if ! systemctl --user is-active hyprpolkitagent.service; then
+    systemctl --user start hyprpolkitagent.service
+  fi
 else
-    notify-send "No polkit agent found" "Please install either polkit-gnome or hyprpolkitagent."
+  notify-send "No polkit agent found" "Please install either polkit-gnome or hyprpolkitagent."
 fi
 
 #clipmanager &
@@ -17,13 +17,13 @@ fi
 nm-applet &
 
 if command -v qpwgraph >/dev/null; then
-    qpwgraph -m &
+  qpwgraph -m &
 fi
 
 HOSTNAME_UPPER=$(hostname | tr 'abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 if echo "$HOSTNAME_UPPER" | grep -q "LAPTOP"; then
-    if [ -x "$HOME/.config/wayland/autostart_laptop.sh" ]; then
-        "$HOME/.config/wayland/autostart_laptop.sh"
-    fi
+  if [ -x "$HOME/.config/desktop/autostart_laptop.sh" ]; then
+    "$HOME/.config/desktop/autostart_laptop.sh"
+  fi
 fi
